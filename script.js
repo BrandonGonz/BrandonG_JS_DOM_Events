a608101c0efd0f539a8a2234c96382c3e1a4ffe5
Brandon Gonzalez <brandongonz.dev1@gmail.com>
2025-04-07 16:43:22 -0700

    added randomColorBtn with RGB background color generator'

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ let getButton = document.getElementById('clickMe');
 getButton.addEventListener('click', function() {
     getButton.innerText = 'You clicked me!';
 })
+
+let colorButton = document.getElementById('randomColorBtn');
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green= Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const randomColor = `rgb(${red}, ${green}, ${blue})`
+
+    document.body.style.backgroundColor = randomColor;
+}
+
+colorButton.addEventListener('click', getRandomColor)
